1d417fa9769af267c6de74415846ef5ce10dfa73
Carlos Moreno <carlos.a.moreno@gmail.com>
2020-12-01 06:37:02 +0100

    BillingServiceTest.kt refactored improving readability

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -11,7 +11,6 @@ import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -22,7 +21,6 @@ class BillingServiceTest {
 
     private val actualInvoice: Invoice = Invoice(1, 1, Money(BigDecimal(100.0), Currency.EUR), InvoiceStatus.PENDING)
     private lateinit var invoiceService: InvoiceService
-    private lateinit var paymentProvider: PaymentProvider
     private lateinit var billingService: BillingService
 
     @BeforeEach
@@ -33,74 +31,83 @@ class BillingServiceTest {
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `given unpaid invoices when the chage is OK then the database is updated `() {
-        paymentProvider = mockPaymentProviderWithResultOk(actualInvoice)
-        billingService = BillingService(paymentProvider, invoiceService)
-
-        whenRunningBillingServices()
+    fun `processing invoices successfully`() {
+        `given a billing service with an invoice that is charged succesfully`()
+        `when billing services processes the invoice`()
         verify(exactly = 1) { invoiceService.updateInvoice(expectedInvoice(InvoiceStatus.PAID)) }
     }
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `given unpaid invoices when the customes doesn't have balance then the database is updated `() {
-        paymentProvider = mockPaymentProviderWithResultNotEnoughFounds(actualInvoice)
-        billingService = BillingService(paymentProvider, invoiceService)
-        whenRunningBillingServices()
+    fun `processing invoices with no balance`() {
+        `given a billing service with a customer with no enough balance`()
+        `when billing services processes the invoice`()
         verify(exactly = 1) { invoiceService.updateInvoice(expectedInvoice(InvoiceStatus.ERROR_NO_BALANCE)) }
     }
 
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `given unpaid invoices when the customes is not found then the database is updated `() {
-        paymentProvider = mockPaymentProviderWithResultCustomerNotFound(actualInvoice)
-        billingService = BillingService(paymentProvider, invoiceService)
-        whenRunningBillingServices()
+    fun `processing invoices with an unexisting customer`() {
+        `given a billing service with a customer not found invoice`()
+        `when billing services processes the invoice`()
         verify(exactly = 1) { invoiceService.updateInvoice(expectedInvoice(InvoiceStatus.ERROR_CUSTOMER_NOT_FOUND)) }
     }
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `given unpaid invoices when the currency is different as expected then the database is updated `() {
-        paymentProvider = mockPaymentProviderWithResultCurrencyMismatch(actualInvoice)
-        billingService = BillingService(paymentProvider, invoiceService)
-        whenRunningBillingServices()
+    fun `processing invoices with currency mismatch`() {
+        `given a billing service with a currency mismatch error invoice`()
+        `when billing services processes the invoice`()
         verify(exactly = 1) { invoiceService.updateInvoice(expectedInvoice(InvoiceStatus.ERROR_CURRENCY_MISMATCH)) }
     }
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `given unpaid invoices when an network error happens then the database is updated `() {
-        paymentProvider = mockPaymentProviderWithResultNetworkException(actualInvoice)
-        billingService = BillingService(paymentProvider, invoiceService)
-        whenRunningBillingServices()
+    fun `processing invoices with network error`() {
+        `given a billing service with a network error invoice`()
+        `when billing services processes the invoice`()
         verify(exactly = 1) { invoiceService.updateInvoice(expectedInvoice(InvoiceStatus.ERROR_NETWORK)) }
     }
 
     @ExperimentalCoroutinesApi
     @Test
-    fun `given unpaid invoices when an unexpected error happens then the database is updated `() {
-        paymentProvider = mockPaymentProviderWithResultUnexpectedError(actualInvoice)
-        billingService = BillingService(paymentProvider, invoiceService)
-        whenRunningBillingServices()
-        verify(exactly = 1) { invoiceService.updateInvoice(expectedInvoice(InvoiceStatus.ERROR_UNKNOWN)) }
+    fun `processing invoices with an unexpected error`() {
+        `given a billing service with an unexpected error invoice`()
+        `when billing services processes the invoice`()
+        verify(exactly = 1) { invoiceService.updateInvoice(any()) }
     }
+
     ///////////////////
     //Auxiliary methods to make the tests easier to read
     ///////////////////
 
     @ExperimentalCoroutinesApi
-    private fun whenRunningBillingServices() {
+    private fun `when billing services processes the invoice`() {
         runBlockingTest {
             billingService.processPendingInvoices()
         }
     }
 
+    private fun `given a billing service with an invoice that is charged succesfully`() {
+        billingService = BillingService(mockPaymentProviderWithResultOk(actualInvoice), invoiceService)
+    }
+    private fun `given a billing service with a customer with no enough balance`() {
+        billingService = BillingService(mockPaymentProviderWithResultNotEnoughFounds(actualInvoice), invoiceService)
+    }
+    private fun `given a billing service with a customer not found invoice`() {
+        billingService = BillingService(mockPaymentProviderWithResultCustomerNotFound(actualInvoice), invoiceService)
+    }
+    private fun `given a billing service with a currency mismatch error invoice`() {
+        billingService = BillingService(mockPaymentProviderWithResultCurrencyMismatch(actualInvoice), invoiceService)
+    }
+    private fun `given a billing service with an unexpected error invoice`() {
+        billingService = BillingService(mockPaymentProviderWithResultUnexpectedError(actualInvoice), invoiceService)
+    }
+    private fun `given a billing service with a network error invoice`() {
+        billingService = BillingService(mockPaymentProviderWithResultNetworkException(actualInvoice), invoiceService)
+    }
     private fun expectedInvoice(invoiceStatus: InvoiceStatus) = actualInvoice.copy(status = invoiceStatus)
-
-    private fun thenDatabaseIsUpdatedWithInvoiceStatus(invoiceService:InvoiceService, invoiceStatus: InvoiceStatus) =
-            verify(exactly = 1) { invoiceService.updateInvoice(expectedInvoice(invoiceStatus)) }
 
     private fun mockInvoiceServiceWithData(invoice: Invoice): InvoiceService {
         return mockk {
